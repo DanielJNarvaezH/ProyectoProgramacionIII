f9fb305af7f69e464c0205963d949f9f752268a4
Juan <[200~juand.garciaa@uqvirtual.edu.co>
2024-09-28 11:27:01 -0500

    Cambio Excepciones

---
+++ src/main/java/co/uniquindio/piii/exceptions/ProductoSinNombreException.java
@@ -0,0 +1,7 @@
+package co.uniquindio.piii.exceptions;
+
+public class ProductoSinNombreException extends Exception {
+    public ProductoSinNombreException(String mensaje) {
+        super(mensaje);
+    }
+}
--- src/main/java/co/uniquindio/piii/exceptions/UsuarioNoRegistradoException.java
+++
@@ -1,7 +0,0 @@
-package co.uniquindio.piii.exceptions;
-
-public class UsuarioNoRegistradoException extends Exception {
-    public UsuarioNoRegistradoException(String mensaje) {
-        super(mensaje);
-    }
-}
